14c58dd68f03f0b0a3d503ab94804704a2368059
Oleh Tarasenko <chipper.og@gmail.com>
2026-01-03 14:42:19 +0000

    User account system
    Foundational user account system. Basic register, login, and logout.

--- app/forms.py
+++ app/forms.py
@@ -1,6 +1,24 @@
 from flask_wtf import FlaskForm
-from wtforms import FloatField, SubmitField
-from wtforms.validators import DataRequired
+from wtforms import FloatField, SubmitField, StringField, PasswordField, BooleanField
+from wtforms.validators import DataRequired, Email, EqualTo, ValidationError
+from app.models import User
+
+class LoginForm(FlaskForm):
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    remember_me = BooleanField('Remember Me')
+    submit = SubmitField('Sign In')
+
+class RegistrationForm(FlaskForm):
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    confirm_password = PasswordField('Repeat Password', validators=[DataRequired(), EqualTo('password')])
+    submit = SubmitField('Register')
+
+    def validate_email(self, email):
+        user = User.query.filter_by(email=email.data).first()
+        if user:
+            raise ValidationError('Please use a different email address.')
 
 class EnergyUseForm(FlaskForm):
     grid_import_kwh = FloatField('Grid Import (kWh)', validators=[DataRequired()])
--- app/routes.py
+++ app/routes.py
@@ -1,12 +1,51 @@
-from flask import render_template, request
-from app import app
-from app.forms import EnergyUseForm
+from flask import render_template, request, flash, redirect, url_for
+from app import app, db
+from app.forms import EnergyUseForm, LoginForm, RegistrationForm
+from flask_login import current_user, login_user, logout_user, login_required
+from app.models import User
+from urllib.parse import urlparse
 
 from app.services.energy_use import EnergyUse
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(email=form.email.data).first()
+        if user is None or not user.check_password(form.password.data):
+            flash('Invalid email or password')
+            return redirect(url_for('login'))
+        login_user(user, remember=form.remember_me.data)
+        next_page = request.args.get('next')
+        if not next_page or urlparse(next_page).netloc != '':
+            next_page = url_for('index')
+        return redirect(next_page)
+    return render_template('login.html', title='Sign In', form=form)
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(email=form.email.data)
+        user.set_password(form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        flash('Congratulations, you are now a registered user!')
+        return redirect(url_for('login'))
+    return render_template('register.html', title='Register', form=form)
 
 @app.route('/energy-use', methods=['GET', 'POST'])
 def energy_use():
